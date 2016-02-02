3dd3045ca36f93f73a7461d80a384881135343c5
Carlos G. Gavidia <cgavidiac@gmail.com>
2016-02-02 10:55:27 +0000

    CSV Utils organization

--- src/main/java/crest/jira/data/miner/csv/CsvUtils.java
+++ src/main/java/crest/jira/data/miner/csv/CsvUtils.java
@@ -2,13 +2,21 @@ package crest.jira.data.miner.csv;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 public class CsvUtils {
+
+  private static final String NEW_LINE_SEPARATOR = "\n";
 
   /**
    * Returns a list of record instances from a CSV file with Header.
@@ -30,5 +38,42 @@ public class CsvUtils {
     }
 
     return csvRecords;
+  }
+
+  /**
+   * Generates a CSV file.
+   * 
+   * @param filePrefix
+   *          Prefix for the generated file.
+   * @param rows
+   *          Rows to include in the file.
+   * @throws IOException
+   *           Problems might arise.
+   */
+  public static void generateCsvFile(String outputFolder, String filePrefix,
+      List<? extends CsvExportSupport> rows) throws IOException {
+
+    if (rows.size() < 1) {
+      throw new RuntimeException(
+          "No rows sent while trying to create file with prefix " + filePrefix);
+    }
+
+    CSVFormat csvFileFormat = CSVFormat.DEFAULT.withRecordSeparator(NEW_LINE_SEPARATOR);
+    BufferedWriter bufferedWriter = new BufferedWriter(
+        new FileWriter(outputFolder + filePrefix + "_" + new Date().getTime() + ".csv"));
+
+    try (CSVPrinter csvPrinter = new CSVPrinter(bufferedWriter, csvFileFormat)) {
+
+      csvPrinter.printRecord(Arrays.copyOf(rows.get(0).getCsvHeader(),
+          rows.get(0).getCsvHeader().length, Object[].class));
+
+      for (CsvExportSupport row : rows) {
+        csvPrinter.printRecord(row.getCsvRecord());
+      }
+
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+
   }
 }
