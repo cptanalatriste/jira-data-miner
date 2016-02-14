5d532d74ef8058afb040abefb22ed8ee127408cf
Carlos G. Gavidia <cgavidiac@gmail.com>
2016-02-14 13:17:45 +0000

    Supporting record of the evolution of inflation ratios

--- src/main/java/crest/jira/data/miner/report/model/ExtendedUser.java
+++ src/main/java/crest/jira/data/miner/report/model/ExtendedUser.java
@@ -1,18 +1,55 @@
 package crest.jira.data.miner.report.model;
 
 import crest.jira.data.retriever.model.User;
+import crest.jira.data.retriever.model.Version;
 
 import org.apache.commons.lang.builder.EqualsBuilder;
 import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.math3.stat.regression.SimpleRegression;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 
 public class ExtendedUser {
 
   private User user;
   private long releaseParticipation;
+  private HashMap<Version, Double> inflationRatios;
 
+  /**
+   * This instance produce user metrics through multiple games.
+   * 
+   * @param reporter
+   *          Original User.
+   */
   public ExtendedUser(User reporter) {
     this.user = reporter;
+    this.inflationRatios = new HashMap<>();
     this.releaseParticipation = 0;
+  }
+
+  /**
+   * Returns a regression instance for the inflation ratios for the tester.
+   * 
+   * @return Regression instance.
+   */
+  public SimpleRegression getRegressionForInflation() {
+    SimpleRegression regression = new SimpleRegression();
+
+    List<Version> versions = new ArrayList<>(inflationRatios.keySet());
+    Collections.sort(versions, new ReleaseDateComparator());
+
+    for (int index = 0; index < versions.size(); index += 1) {
+      regression.addData(index, inflationRatios.get(versions.get(index)));
+    }
+
+    return regression;
+  }
+
+  public HashMap<Version, Double> getInflationRatios() {
+    return inflationRatios;
   }
 
   public void reportParticipation() {
